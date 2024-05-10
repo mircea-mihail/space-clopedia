473363079e4df91137b2510a8cd394f610443793
Ioan Anghel <ioan.anghel123@gmail.com>
2024-05-10 10:23:55 +0300

    Added inital classes w/ constructors: Users, Articles, Domains

---
+++ SpaceClopedia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpaceClopedia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SpaceClopedia/Models/Utilizator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SpaceClopedia.Models;
+
+namespace SpaceClopedia.Models
+{
+    public class Utilizator
+    {
+        public int Id { get; set; }
+        public string NumeUtilizator { get; set; }
+        //Rol -- 0(admin), 1(moderator), 2(utlizator obisnuit)
+        public string Parola { get; set; }
+        public int Rol { get; set; }
+        public DateTime DataInregistrare { get; set; }
+
+        public Utilizator(int _Id, string _NumeUtilizator, string _Parola, int _Rol, DateTime _DataInregistrare)
+        {
+            Id = _Id;
+            NumeUtilizator = _NumeUtilizator;
+            Parola = _Parola;
+            Rol = _Rol;
+            DataInregistrare = _DataInregistrare;
+        }
+
+    }
+}
